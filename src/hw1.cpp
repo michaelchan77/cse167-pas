9e8a76cc0f180f2167c2cf1efdf5a03e745392a8
michaelchan77 <97709300+michaelchan77@users.noreply.github.com>
2023-10-08 18:00:48 -0700

    refactor rasterize circle code

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -3,6 +3,39 @@
 
 using namespace hw1;
 
+struct rasterize_shape_op {
+    // Credit: Function skeleton from @bwu on Discord 
+    // (CSE167 FA23 server, #homework-help channel)
+
+    Image3 &img;
+    // constructor
+    rasterize_shape_op(Image3 &image) : img(image) {
+    }
+    void operator()(const Circle &circle) {
+        // bounding box
+        Real minX = std::max(circle.center.x - circle.radius, Real(0.));
+        Real minY = std::max(circle.center.y - circle.radius, Real(0.));
+        Real maxX = std::min(circle.center.x + circle.radius, Real(img.width));
+        Real maxY = std::min(circle.center.y + circle.radius, Real(img.height));
+        Vector2 pMin = Vector2{minX, minY};
+        Vector2 pMax = Vector2{maxX, maxY};
+        // rasterize
+        for (int y = pMin.y; y < pMax.y; y++) {
+            for (int x = pMin.x; x < pMax.x; x++) {
+                Vector2 p = Vector2{x + Real(0.5), y + Real(0.5)};
+                if(length(p - circle.center) < circle.radius)
+                    img(x, y) = circle.color;
+            }
+        }
+    }
+    void operator()(const Rectangle &rectangle) {
+        // ADD
+    }
+    void operator()(const Triangle &triangle) {
+        // ADD
+    }
+}; 
+
 Image3 hw_1_1(const std::vector<std::string> &params) {
     // Homework 1.1: render a circle at the specified
     // position, with the specified radius and color.
@@ -12,6 +45,7 @@ Image3 hw_1_1(const std::vector<std::string> &params) {
     Vector2 center = Vector2{img.width / 2 + Real(0.5), img.height / 2 + Real(0.5)};
     Real radius = 100.0;
     Vector3 color = Vector3{1.0, 0.5, 0.5};
+    // get params
     for (int i = 0; i < (int)params.size(); i++) {
         if (params[i] == "-center") {
             Real x = std::stof(params[++i]);
@@ -26,7 +60,7 @@ Image3 hw_1_1(const std::vector<std::string> &params) {
             color = Vector3{r, g, b};
         }
     }
-
+    // rasterize
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
             Vector2 curr = Vector2{x + Real(0.5), y + Real(0.5)};
@@ -56,18 +90,9 @@ Image3 hw_1_2(const std::vector<std::string> &params) {
             img(x, y) = scene.background;
         }
     }
-    // rasterization
-    for (Circle c : scene.objects) {
-        // bounding box
-        Vector2 upperLeft = Vector2{std::max(c.center.x - c.radius, Real(0.)), std::max(c.center.y - c.radius, Real(0.))};
-        Vector2 lowerRight = Vector2{std::min(c.center.x + c.radius, Real(img.width)), std::min(c.center.y + c.radius, Real(img.height))};
-        for (int y = upperLeft.y; y < lowerRight.y; y++) {
-            for (int x = upperLeft.x; x < lowerRight.x; x++) {
-                Vector2 curr = Vector2{x + Real(0.5), y + Real(0.5)};
-                if(length(curr - c.center) < c.radius)
-                    img(x, y) = c.color;
-            }
-        }
+    // rasterize
+    for (const Shape &shape : scene.objects) {
+        std::visit(rasterize_shape_op(img), shape);
     }
     return img;
 }
@@ -82,11 +107,15 @@ Image3 hw_1_3(const std::vector<std::string> &params) {
     std::cout << scene << std::endl;
 
     Image3 img(scene.resolution.x, scene.resolution.y);
-
+    // initialize background
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            img(x, y) = scene.background;
         }
+    }
+    // rasterize
+    for (const auto &shape : scene.shapes) {
+        std::visit(rasterize_shape_op(img), shape);
     }
     return img;
 }
