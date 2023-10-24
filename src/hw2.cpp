aaac07e337fc7ec06a8b3c5ccbaa385be9bfacf8
michaelchan77 <97709300+michaelchan77@users.noreply.github.com>
2023-10-23 20:48:08 -0700

    hw 2_1 rename vars add lambdas

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -41,9 +41,9 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     Image3 img(640 /* width */, 480 /* height */);
     Image3 img_4x(4*img.width, 4*img.height);
 
-    Vector3 p0{0, 0, -1};
-    Vector3 p1{1, 0, -1};
-    Vector3 p2{0, 1, -1};
+    Vector3 p0_3D{0, 0, -1};
+    Vector3 p1_3D{1, 0, -1};
+    Vector3 p2_3D{0, 1, -1};
     Real s = 1; // scaling factor of the view frustrum
     Real a = Real(img.width)/Real(img.height); // aspect ratio
     Vector3 color = Vector3{1.0, 0.5, 0.5};
@@ -53,17 +53,17 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
         if (params[i] == "-s") {
             s = std::stof(params[++i]);
         } else if (params[i] == "-p0") {
-            p0.x = std::stof(params[++i]);
-            p0.y = std::stof(params[++i]);
-            p0.z = std::stof(params[++i]);
+            p0_3D.x = std::stof(params[++i]);
+            p0_3D.y = std::stof(params[++i]);
+            p0_3D.z = std::stof(params[++i]);
         } else if (params[i] == "-p1") {
-            p1.x = std::stof(params[++i]);
-            p1.y = std::stof(params[++i]);
-            p1.z = std::stof(params[++i]);
+            p1_3D.x = std::stof(params[++i]);
+            p1_3D.y = std::stof(params[++i]);
+            p1_3D.z = std::stof(params[++i]);
         } else if (params[i] == "-p2") {
-            p2.x = std::stof(params[++i]);
-            p2.y = std::stof(params[++i]);
-            p2.z = std::stof(params[++i]);
+            p2_3D.x = std::stof(params[++i]);
+            p2_3D.y = std::stof(params[++i]);
+            p2_3D.z = std::stof(params[++i]);
         } else if (params[i] == "-color") {
             Real r = std::stof(params[++i]);
             Real g = std::stof(params[++i]);
@@ -75,47 +75,49 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     }
     
     // project vertices to image plane (camera space)
-    Vector2 p0_2D = Vector2{-Real(p0.x)/Real(p0.z), -Real(p0.y)/Real(p0.z)};
-    Vector2 p1_2D = Vector2{-Real(p1.x)/Real(p1.z), -Real(p1.y)/Real(p1.z)};
-    Vector2 p2_2D = Vector2{-Real(p2.x)/Real(p2.z), -Real(p2.y)/Real(p2.z)};
+    auto pToCam = [](const Vector3 &v) { 
+        return Vector2{-v.x/v.z, -v.y/v.z}; 
+    };
+    Vector2 p0_2D = pToCam(p0_3D), p1_2D = pToCam(p1_3D), p2_2D = pToCam(p2_3D);
     // set view frustum & near clipping plane
-    bool outOfView = p0_2D.x<-s*a && p0_2D.x>s*a && p0_2D.y<-s && p0_2D.x>s
-                  && p1_2D.x<-s*a && p1_2D.x>s*a && p1_2D.y<-s && p1_2D.x>s
-                  && p2_2D.x<-s*a && p2_2D.x>s*a && p2_2D.y<-s && p2_2D.x>s
-                  && -p0.z<z_near && -p1.z<z_near && -p2.z<z_near;
+    auto clip = [&](const Vector2 &v) { 
+        return v.x < -s*a && v.x > s*a && v.y < -s && v.y > s; 
+    };
+    bool p0_clipped = clip(p0_2D), p1_clipped = clip(p1_2D), p2_clipped = clip(p2_2D);
+    bool near_clip = -p0_3D.z<z_near && -p1_3D.z<z_near && -p2_3D.z<z_near;
     // convert from camera space to 4x image space
-    p0_2D.x = img_4x.width*(p0_2D.x+s*a)/(2*s*a);
-    p0_2D.y = -img_4x.height*(p0_2D.y-s)/(2*s);
-    p1_2D.x = img_4x.width*(p1_2D.x+s*a)/(2*s*a);
-    p1_2D.y = -img_4x.height*(p1_2D.y-s)/(2*s);
-    p2_2D.x = img_4x.width*(p2_2D.x+s*a)/(2*s*a);
-    p2_2D.y = -img_4x.height*(p2_2D.y-s)/(2*s);
+    auto camToImg = [&](const Vector2 &v) { 
+        return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), -img_4x.height*(v.y-s)/(2*s)}; 
+    };
+    Vector2 p0 = camToImg(p0_2D), p1 = camToImg(p1_2D), p2 = camToImg(p2_2D);
     // rasterize 4x image
     for (int y = 0; y < img_4x.height; y++) {
         for (int x = 0; x < img_4x.width; x++) {
             img_4x(x, y) = background;
         }
     }
-    if (!outOfView) {
+    if (!(p0_clipped || p1_clipped || p2_clipped || near_clip)) {
         // bounding box
-        Real minX = std::min({p0_2D.x, p1_2D.x, p2_2D.x});
-        Real maxX = std::max({p0_2D.x, p1_2D.x, p2_2D.x});
-        Real minY = std::min({p0_2D.y, p1_2D.y, p2_2D.y});
-        Real maxY = std::max({p0_2D.y, p1_2D.y, p2_2D.y});
+        Real minX = std::min({p0.x, p1.x, p2.x});
+        Real maxX = std::max({p0.x, p1.x, p2.x});
+        Real minY = std::min({p0.y, p1.y, p2.y});
+        Real maxY = std::max({p0.y, p1.y, p2.y});
         Vector2 pMin = Vector2{std::max(minX, Real(0)), std::max(minY, Real(0))};
         Vector2 pMax = Vector2{std::min(maxX, Real(img_4x.width)), 
                                std::min(maxY, Real(img_4x.height))};       
         // edges and normals
-        Vector2 e01 = p1_2D-p0_2D, e12 = p2_2D-p1_2D, e20 = p0_2D-p2_2D;
-        auto normal = [](const Vector2 &v) { return Vector2{v.y, -v.x}; };
+        Vector2 e01 = p1 - p0, e12 = p2 - p1, e20 = p0 - p2;
+        auto normal = [](const Vector2 &v) { 
+            return Vector2{v.y, -v.x}; 
+        };
         Vector2 n01 = normal(e01), n12 = normal(e12), n20 = normal(e20);
         // rasterize
         for (int y = pMin.y; y < pMax.y; y++) {
             for (int x = pMin.x; x < pMax.x; x++) {
                 Vector2 pixel_center{x + Real(0.5), y + Real(0.5)};
-                bool s01 = dot(pixel_center - p0_2D, n01) > 0;
-                bool s12 = dot(pixel_center - p1_2D, n12) > 0;
-                bool s20 = dot(pixel_center - p2_2D, n20) > 0;
+                bool s01 = dot(pixel_center - p0, n01) > 0;
+                bool s12 = dot(pixel_center - p1, n12) > 0;
+                bool s20 = dot(pixel_center - p2, n20) > 0;
                 if ((s01 && s12 && s20) || (!s01 && !s12 && !s20)) {
                     img_4x(x, y) = color;
                 }
