5eac10b076f88784ff08975dfc4b8479bdf13d37
michaelchan77 <97709300+michaelchan77@users.noreply.github.com>
2023-10-26 21:57:34 -0700

    2_1 copy from part 2 and redo images

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -9,29 +9,28 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     Image3 img(640 /* width */, 480 /* height */);
     Image3 img_4x(4*img.width, 4*img.height);
 
-    Vector3 p0_3D{0, 0, -1};
-    Vector3 p1_3D{1, 0, -1};
-    Vector3 p2_3D{0, 1, -1};
+    Vector3 p0{0, 0, -1};
+    Vector3 p1{1, 0, -1};
+    Vector3 p2{0, 1, -1};
     Real s = 1; // scaling factor of the view frustrum
     Real a = Real(img.width)/Real(img.height); // aspect ratio
     Vector3 color = Vector3{1.0, 0.5, 0.5};
-    Vector3 background = Vector3{0.5, 0.5, 0.5};
     Real z_near = 1e-6; // distance of the near clipping plane
     for (int i = 0; i < (int)params.size(); i++) {
         if (params[i] == "-s") {
             s = std::stof(params[++i]);
         } else if (params[i] == "-p0") {
-            p0_3D.x = std::stof(params[++i]);
-            p0_3D.y = std::stof(params[++i]);
-            p0_3D.z = std::stof(params[++i]);
+            p0.x = std::stof(params[++i]);
+            p0.y = std::stof(params[++i]);
+            p0.z = std::stof(params[++i]);
         } else if (params[i] == "-p1") {
-            p1_3D.x = std::stof(params[++i]);
-            p1_3D.y = std::stof(params[++i]);
-            p1_3D.z = std::stof(params[++i]);
+            p1.x = std::stof(params[++i]);
+            p1.y = std::stof(params[++i]);
+            p1.z = std::stof(params[++i]);
         } else if (params[i] == "-p2") {
-            p2_3D.x = std::stof(params[++i]);
-            p2_3D.y = std::stof(params[++i]);
-            p2_3D.z = std::stof(params[++i]);
+            p2.x = std::stof(params[++i]);
+            p2.y = std::stof(params[++i]);
+            p2.z = std::stof(params[++i]);
         } else if (params[i] == "-color") {
             Real r = std::stof(params[++i]);
             Real g = std::stof(params[++i]);
@@ -41,55 +40,51 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
             z_near = std::stof(params[++i]);
         }
     }
-    // project vertices to image plane (camera space)
-    auto pToCam = [](const Vector3 &v) { 
-        return Vector2{-v.x/v.z, -v.y/v.z}; 
-    };
-    Vector2 p0_2D = pToCam(p0_3D), 
-            p1_2D = pToCam(p1_3D), 
-            p2_2D = pToCam(p2_3D);
-    // convert from camera space to 4x image space
-    auto camToImg = [&](const Vector2 &v) { 
-        return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), 
-                      -img_4x.height*(v.y-s)/(2*s)}; 
-    };
-    Vector2 p0 = camToImg(p0_2D), 
-            p1 = camToImg(p1_2D), 
-            p2 = camToImg(p2_2D);
-    // rasterize 4x image
+    // set 4x image background
     for (int y = 0; y < img_4x.height; y++) {
         for (int x = 0; x < img_4x.width; x++) {
-            img_4x(x, y) = background;
+            img_4x(x, y) = Vector3{0.5, 0.5, 0.5};
         }
     }
-    // within view frustum & near clipping plane
-    auto outOfView = [&](const Vector2 &v) { 
-        return v.x < -s*a && v.x > s*a && v.y < -s && v.y > s; 
-    };
-    bool b0 = outOfView(p0_2D), b1 = outOfView(p1_2D), b2 = outOfView(p2_2D);
-    bool bNear = -p0_3D.z<z_near && -p1_3D.z<z_near && -p2_3D.z<z_near;
-    if (!(b0 || b1 || b2 || bNear)) {
-        // bounding box
-        Real minX = std::min({p0.x, p1.x, p2.x});
-        Real maxX = std::max({p0.x, p1.x, p2.x});
-        Real minY = std::min({p0.y, p1.y, p2.y});
-        Real maxY = std::max({p0.y, p1.y, p2.y});
-        Vector2 pMin = Vector2{std::max(minX, Real(0)), std::max(minY, Real(0))};
-        Vector2 pMax = Vector2{std::min(maxX, Real(img_4x.width)), 
-                               std::min(maxY, Real(img_4x.height))};       
-        // edges and normals
-        Vector2 e01 = p1 - p0, e12 = p2 - p1, e20 = p0 - p2;
+    // if inside near clipping plane
+    if (!(-p0.z<z_near && -p1.z<z_near && -p2.z<z_near)) {
+        // project vertices to image plane (camera space)
+        auto pToCam = [](const Vector3 &v) { 
+            return Vector2{-v.x/v.z, -v.y/v.z}; 
+        };
+        Vector2 p0_cam = pToCam(p0), 
+                p1_cam = pToCam(p1), 
+                p2_cam = pToCam(p2);
+        // convert from camera space to 4x image space
+        auto camToImg = [&](const Vector2 &v) { 
+            return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), 
+                        -img_4x.height*(v.y-s)/(2*s)}; 
+        };
+        Vector2 p0_img = camToImg(p0_cam), 
+                p1_img = camToImg(p1_cam), 
+                p2_img = camToImg(p2_cam);
+        // define bounding box
+        Real minX = max(std::min({p0_img.x, p1_img.x, p2_img.x}), Real(0));
+        Real minY = max(std::min({p0_img.y, p1_img.y, p2_img.y}), Real(0));
+        Real maxX = min(std::max({p0_img.x, p1_img.x, p2_img.x}), Real(img_4x.width));
+        Real maxY = min(std::max({p0_img.y, p1_img.y, p2_img.y}), Real(img_4x.height));
+        // define edges and normals
+        Vector2 e01 = p1_img - p0_img, 
+                e12 = p2_img - p1_img, 
+                e20 = p0_img - p2_img;
         auto normal = [](const Vector2 &v) { 
             return Vector2{v.y, -v.x}; 
         };
-        Vector2 n01 = normal(e01), n12 = normal(e12), n20 = normal(e20);
-        // rasterize
-        for (int y = pMin.y; y < pMax.y; y++) {
-            for (int x = pMin.x; x < pMax.x; x++) {
+        Vector2 n01 = normal(e01), 
+                n12 = normal(e12), 
+                n20 = normal(e20);
+        // check if points hit triangle
+        for (int y = minY; y < maxY; y++) {
+            for (int x = minX; x < maxX; x++) {
                 Vector2 pixel_center{x + Real(0.5), y + Real(0.5)};
-                bool s01 = dot(pixel_center - p0, n01) > 0;
-                bool s12 = dot(pixel_center - p1, n12) > 0;
-                bool s20 = dot(pixel_center - p2, n20) > 0;
+                bool s01 = dot(pixel_center - p0_img, n01) > 0;
+                bool s12 = dot(pixel_center - p1_img, n12) > 0;
+                bool s20 = dot(pixel_center - p2_img, n20) > 0;
                 if ((s01 && s12 && s20) || (!s01 && !s12 && !s20)) {
                     img_4x(x, y) = color;
                 }
