a6f732c18f990430ad5012ea20d06968534d9a92
michaelchan77 <97709300+michaelchan77@users.noreply.github.com>
2023-10-26 21:40:21 -0700

    2_2 finish base from part 1 and fix ssaa

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -50,9 +50,12 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
             p2_2D = pToCam(p2_3D);
     // convert from camera space to 4x image space
     auto camToImg = [&](const Vector2 &v) { 
-        return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), -img_4x.height*(v.y-s)/(2*s)}; 
+        return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), 
+                      -img_4x.height*(v.y-s)/(2*s)}; 
     };
-    Vector2 p0 = camToImg(p0_2D), p1 = camToImg(p1_2D), p2 = camToImg(p2_2D);
+    Vector2 p0 = camToImg(p0_2D), 
+            p1 = camToImg(p1_2D), 
+            p2 = camToImg(p2_2D);
     // rasterize 4x image
     for (int y = 0; y < img_4x.height; y++) {
         for (int x = 0; x < img_4x.width; x++) {
@@ -63,9 +66,7 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     auto outOfView = [&](const Vector2 &v) { 
         return v.x < -s*a && v.x > s*a && v.y < -s && v.y > s; 
     };
-    bool b0 = outOfView(p0_2D), 
-         b1 = outOfView(p1_2D), 
-         b2 = outOfView(p2_2D);
+    bool b0 = outOfView(p0_2D), b1 = outOfView(p1_2D), b2 = outOfView(p2_2D);
     bool bNear = -p0_3D.z<z_near && -p1_3D.z<z_near && -p2_3D.z<z_near;
     if (!(b0 || b1 || b2 || bNear)) {
         // bounding box
@@ -99,10 +100,13 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
             Vector3 color = {0, 0, 0};
-            for (int i = 0; i < 3; i++) {
-                color += img_4x(4*x+i, 4*y+i);
+            Real n = 4;
+            for (int dx = 0; dx < n; dx++) {
+                for (int dy = 0; dy < n; dy++) {
+                    color += img_4x(n*x+dx, n*y+dy);
+                }
             }
-            img(x,y) = color/Real(4);
+            img(x,y) = color/(n*n);
         }
     }
     return img;
@@ -129,22 +133,76 @@ Image3 hw_2_2(const std::vector<std::string> &params) {
     }
 
     TriangleMesh mesh = meshes[scene_id];
-    UNUSED(mesh); // silence warning, feel free to remove this
 
-    // rasterize 4x image
+    // set 4x image background
     for (int y = 0; y < img_4x.height; y++) {
         for (int x = 0; x < img_4x.width; x++) {
             img_4x(x, y) = Vector3{0.5, 0.5, 0.5};
         }
     }
+    // for each 3D triangle
+    for (int i = 0; i < mesh.faces.size(); i++) {
+        Vector3 p0 = mesh.vertices[mesh.faces[i].x], 
+                p1 = mesh.vertices[mesh.faces[i].y], 
+                p2 = mesh.vertices[mesh.faces[i].z];
+        // if inside near clipping plane
+        if (!(-p0.z<z_near && -p1.z<z_near && -p2.z<z_near)) {
+            // project vertices to image plane (camera space)
+            auto pToCam = [](const Vector3 &v) { 
+                return Vector2{-v.x/v.z, -v.y/v.z}; 
+            };
+            Vector2 p0_cam = pToCam(p0), 
+                    p1_cam = pToCam(p1), 
+                    p2_cam = pToCam(p2);
+            // convert from camera space to 4x image space
+            auto camToImg = [&](const Vector2 &v) { 
+                return Vector2{img_4x.width*(v.x+s*a)/(2*s*a), 
+                            -img_4x.height*(v.y-s)/(2*s)}; 
+            };
+            Vector2 p0_img = camToImg(p0_cam), 
+                    p1_img = camToImg(p1_cam), 
+                    p2_img = camToImg(p2_cam);
+            // define bounding box
+            Real minX = max(std::min({p0_img.x, p1_img.x, p2_img.x}), Real(0));
+            Real minY = max(std::min({p0_img.y, p1_img.y, p2_img.y}), Real(0));
+            Real maxX = min(std::max({p0_img.x, p1_img.x, p2_img.x}), Real(img_4x.width));
+            Real maxY = min(std::max({p0_img.y, p1_img.y, p2_img.y}), Real(img_4x.height));
+            // define edges and normals
+            Vector2 e01 = p1_img - p0_img, 
+                    e12 = p2_img - p1_img, 
+                    e20 = p0_img - p2_img;
+            auto normal = [](const Vector2 &v) { 
+                return Vector2{v.y, -v.x}; 
+            };
+            Vector2 n01 = normal(e01), 
+                    n12 = normal(e12), 
+                    n20 = normal(e20);
+            // check if points hit triangle
+            for (int y = minY; y < maxY; y++) {
+                for (int x = minX; x < maxX; x++) {
+                    Vector2 pixel_center{x + Real(0.5), y + Real(0.5)};
+                    bool s01 = dot(pixel_center - p0_img, n01) > 0;
+                    bool s12 = dot(pixel_center - p1_img, n12) > 0;
+                    bool s20 = dot(pixel_center - p2_img, n20) > 0;
+                    if ((s01 && s12 && s20) || (!s01 && !s12 && !s20)) {
+                        img_4x(x, y) = mesh.face_colors[i];
+                    }
+                }
+            }
+        }
+        
+    }
     // convert to 1x image
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
             Vector3 color = {0, 0, 0};
-            for (int i = 0; i < 3; i++) {
-                color += img_4x(4*x+i, 4*y+i);
+            Real n = 4;
+            for (int dx = 0; dx < n; dx++) {
+                for (int dy = 0; dy < n; dy++) {
+                    color += img_4x(n*x+dx, n*y+dy);
+                }
             }
-            img(x,y) = color/Real(4);
+            img(x,y) = color/(n*n);
         }
     }
     return img;
